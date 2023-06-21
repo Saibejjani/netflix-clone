199806cf856cad596ca40f31260ff8715e3b396b
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2023-06-21 21:56:52 +0530

    banner is added

--- src/Banner.js
+++ src/Banner.js
@@ -2,18 +2,32 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = () => {
+  function truncate(string, n) {
+    return string?.length > n ? string.substring(0, n - 1) + '...' : string;
+  }
   return (
     <header
       className="banner"
       style={{
         backgroundSize: 'cover',
-        backgroundImage: `url("https://d1csarkz8obe9u.cloudfront.net/posterpreviews/netflix-movie-series-download-template-design-3a7d8e00db6fffde55bb1c0e8a65eb79_screen.jpg")`,
+        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
         backgroundPosition: 'center center',
       }}
     >
       <div className="banner__content">
-        <h1 className="banner_title">movie name</h1>
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h2 className="banner__description">
+          {truncate(
+            'This is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie descriptionThis is a movie description',
+            150
+          )}
+        </h2>
       </div>
+      <div className="banner__fadeBottom" />
     </header>
   );
 };
