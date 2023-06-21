734b6e885949d7dd193b124710e591cecce9dbee
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2023-06-21 19:22:25 +0530

    intial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAMwuJniGy1wZu_s2nPNOVBPxFwZ1wMNrg',
+  authDomain: 'netflix-clone-bc056.firebaseapp.com',
+  projectId: 'netflix-clone-bc056',
+  storageBucket: 'netflix-clone-bc056.appspot.com',
+  messagingSenderId: '643135046099',
+  appId: '1:643135046099:web:a24dd02d7e056d31ba2f26',
+};
